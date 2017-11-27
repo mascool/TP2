a433bc4558ab397005cf230fbb4e082103776adb
anthonymartin <anthom_13@hotmail.com>
2017-11-27 17:19:01 -0500

    allo

--- app/src/main/java/local/tp2/MainActivity.java
+++ app/src/main/java/local/tp2/MainActivity.java
@@ -1,9 +1,48 @@
 package local.tp2;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Montre les items
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.apropos:
+                Context context = getApplicationContext();
+                LayoutInflater inflater = getLayoutInflater();
+
+                View toastRoot = inflater.inflate(R.layout.a_propos, null);
+
+                Toast toast = new Toast(context);
+
+                toast.setView(toastRoot);
+                toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
