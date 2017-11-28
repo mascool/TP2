03db6b3b01d5e2bff2718a63a579c266b0878b7f
anthonymartin <anthom_13@hotmail.com>
2017-11-28 00:18:57 -0500

    allo

--- app/src/main/java/local/tp2/MainActivity.java
+++ app/src/main/java/local/tp2/MainActivity.java
@@ -1,52 +1,109 @@
 package local.tp2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_bar, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    //Montre les items
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch(item.getItemId()) {
-            case R.id.apropos:
-                Context context = getApplicationContext();
-                LayoutInflater inflater = getLayoutInflater();
-
-                View toastRoot = inflater.inflate(R.layout.a_propos, null);
-
-                Toast toast = new Toast(context);
-
-                toast.setView(toastRoot);
-                toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
-                toast.setDuration(Toast.LENGTH_LONG);
-                toast.show();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RecyclerView rv = (RecyclerView) findViewById(R.id.list);
+
+        //Arraylist des Notes
+        ArrayList<NoteActivity> noteActivities = new ArrayList<NoteActivity>();
+        noteActivities.add(new NoteActivity("Nouvelle Note", df.format(Calendar.getInstance().getTime()),""));
+
+        //set le RecyclerView
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new MyAdapter(noteActivities));
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        private ArrayList<NoteActivity> noteActivities;
+        public MyAdapter(ArrayList<NoteActivity> noteActivities) {this.noteActivities = noteActivities;
+        }
+
+        @Override
+        //Compte le numero du bouton
+        public int getItemCount() {return noteActivities.size();}
+
+        @Override
+        //Layout dans le boutons
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(R.layout.case_liste, parent,false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            NoteActivity noteActivity = noteActivities.get(position);
+            holder.display(noteActivity);
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+
+            private NoteActivity currentNote;
+            private final TextView titre;
+            private final TextView date;
+            private final TextView ligne1;
+
+
+
+            public MyViewHolder(final View itemView) {
+                super(itemView);
+
+                titre = ((TextView) itemView.findViewById(R.id.titre));
+                date = ((TextView) itemView.findViewById(R.id.date));
+                ligne1 = ((TextView) itemView.findViewById(R.id.ligne1));
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        //OUVRIR LA NOTE OU ON A CLIQUÉ
+
+
+
+
+
+                    }
+                });
+            }
+
+            //Afficher l'unité (dans le bouton)
+            public void display(NoteActivity noteActivityy) {
+                currentNote = noteActivityy;
+                titre.setText(noteActivityy.getTitre());
+                date.setText(noteActivityy.getDate());
+                ligne1.setText(noteActivityy.getLigne1());
+            }
+        }
+
+
     }
 }
--- app/src/main/java/local/tp2/NoteActivity.java
+++ app/src/main/java/local/tp2/NoteActivity.java
@@ -1,8 +1,111 @@
 package local.tp2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
 /**
  * Created by anthonymartin on 17-11-27.
  */
 
-public class NoteActivity {
+public class NoteActivity extends AppCompatActivity {
+
+    private String titre;
+    private String date;
+    private String ligne1;
+
+    public NoteActivity(String titre, String date, String ligne1) {
+
+        this.titre = titre;
+        this.date = date;
+        this.ligne1 = ligne1;
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note);
+
+        //Initialisation des Boutons cliquables
+        ImageButton supprimer = (ImageButton)findViewById(R.id.supprimer);
+        ImageButton reglage = (ImageButton)findViewById(R.id.reglage);
+        ImageButton ajouter = (ImageButton)findViewById(R.id.ajouter);
+
+        //Listener pour supprimer la note
+        supprimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //SUPPRIMER LA NOTE
+
+            }
+        });
+
+        //Listener pour acceder aux reglages
+        reglage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //ACCEDER AUX REGLAGES
+            }
+        });
+
+        //Listener pour ajouter une nouvelle note
+        ajouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //AJOUTER UNE NOUVELLE NOTE
+            }
+        });
+
+
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre){
+        this.titre = titre;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(){
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy, HH:mm");
+        String date = df.format(Calendar.getInstance().getTime());
+        this.date = date;
+    }
+
+    public String getLigne1() {
+        return ligne1;
+    }
+
+    public void setLigne1(NoteActivity currentNote){
+
+        /////////////COMMENT TROUVER LA PREMIERE LIGNE??? YÉ RENDU MINUIT PI CA ME TENTE PAS D'ESSAYER DE TROUVER//////////////
+
+        //currentNote.
+
+        this.ligne1 = ligne1;
+    }
+
 }
